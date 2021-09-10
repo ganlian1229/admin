eb37532953682096dbb58a3d56638c3e7dcbcab7
gl <1033970489@qq.com>
2021-09-10 17:49:41 +0800

    更新 common.js

--- src/common/common.js
+++ src/common/common.js
@@ -1,7 +1,7 @@
 //公用方法
 export default class CommonFun {
     // checkedAll变量传过来  arr：需要全选的数组 addArr：全选之后的数组（做删除或者撤回操作的数组） variable：需要做删除或者撤回的变量
-    static checkedAllFun(checkedAll, arr, addArr, variable) {
+    static checkedAllFun (checkedAll, arr, addArr, variable) {
         console.log(checkedAll)
         if (checkedAll) {
             addArr = [];
@@ -18,7 +18,7 @@ export default class CommonFun {
         checkedAll = !checkedAll;
         return addArr;
     }
-    static checkedFun(item, addArr, variable) {
+    static checkedFun (item, addArr, variable) {
         // console.log(item);
         if (item.checked) {
             addArr.push(item[variable]);
@@ -32,27 +32,30 @@ export default class CommonFun {
     }
 
     //对象、数组深拷贝
-    static deepCopy(obj) {
-        var newobj = {}
-        //判断是否为null
-        if (obj === null) return null;
-        for (var key in obj) {
-            //判断数组
-            if ({}.toString.call(obj) === "[object Array]") {
-                var newarr = [];
-                newarr = obj.slice()
-                return newarr
+    static deepCopy (source) {
+        if (!source) return
+        let target;
+        if (typeof source === 'object') {
+            // 根据source类型判断是新建一个数组还是对象
+            target = Array.isArray(source) ? [] : {}
+            // 遍历source，并且判断是source的属性才拷贝
+            for (let key in source) {
+                if (source.hasOwnProperty(key)) {
+                    if (typeof source[key] !== 'object') {
+                        target[key] = source[key]
+                    } else {
+                        // 如果内部属性存在复杂数据类型，使用递归实现深拷贝
+                        target[key] = this.deepCopy(source[key])
+                    }
+                }
             }
-            if (typeof obj[key] !== "object") {
-                newobj[key] = obj[key]
-            } else {
-                newobj[key] = this.deepCopy(obj[key])
-            }
+        } else {
+            target = source
         }
-        return newobj
+        return target
     }
     //增强arr indexOf方法
-    static increaseArrIndexOf(arr, variable, value) {
+    static increaseArrIndexOf (arr, variable, value) {
         for (let i = 0; i < arr.length; i++) {
             if (arr[i][variable] == value) {
                 return i;
@@ -61,7 +64,7 @@ export default class CommonFun {
         return -1;
     }
     //数组去重方法
-    static delArr(arr) {
+    static delArr (arr) {
         var newArr = [];
         for (var i = 0; i < arr.length; i++) {
             let item = arr[i];
